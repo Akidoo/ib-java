f8cf86b10b075e9187b1751d15a37d4db4678c6f
Akidoo <plunkels@gmail.com>
2023-02-20 21:56:35 +0800

    update

---
+++ array_Practice/array_Practice.java
@@ -0,0 +1,32 @@
+
+
+
+public class array_Practice {
+	
+	public static void main (String[] args) {
+		
+		 int ages[] = {4,69,20,16,19};
+		 double heights[] = {1.02,1.9,1.79,1.95, 1.88};
+		 
+		 // int ages[0] = 4;
+		 // int ages[1] = 69;
+		 // int ages[2] = 20;
+		 // int ages[3] = 16;
+		 // int ages[4] = 19;
+		 
+		 // double heights[0] = 1.02;
+		 // double heights[1] = 1.9;
+		 // double heights[2] = 1.79;
+		 // double heights[3] = 1.95;
+		 // double heights[4] = 1.88;
+		 
+		 System.out.println("The first person is " + ages[0] + " years old and " + heights[0] + "m tall");
+		 System.out.println("The second person is " + ages[1] + " years old and " + heights[1] + "m tall");
+		 System.out.println("The third person is " + ages[2] + " years old and " + heights[2] + "m tall");
+		 System.out.println("The fourth person is " + ages[3] + " years old and " + heights[3] + "m tall");
+		 System.out.println("The fifth person is " + ages[4] + " years old and " + heights[4] + "m tall");
+
+		
+	}
+}
+
