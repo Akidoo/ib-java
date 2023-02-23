1deb64db7c16550fb9f60e148795bc7d78887399
Akidoo <plunkels@gmail.com>
2023-02-23 11:32:10 +0800

    Update

--- stringsPractice/stringsPractice.java
+++ stringsPractice/stringsPractice.java
@@ -4,17 +4,20 @@
 public class stringsPractice {
 	
 	public static void main (String[] args) {
-		
+		//Input
 		String ss = IBIO.input("enter your name: ");
 		char[] xx = ss.toCharArray();
 		
+		//Print characters in the String
 		for(int i = 0; i<ss.length(); i++){
 			System.out.println(xx[i]);
 			
 			}
 			
-			//Figure out how to reverse array next
-		
+			//Reverse String
+		for(int i = ss.length()-1; i>-1; i--){
+			System.out.println(xx[i]);
+		}
 	}
 }
 
