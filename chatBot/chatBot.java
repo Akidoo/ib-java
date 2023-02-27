9b57582c68345213890906b49997b1b4ac3363c6
Akidoo <plunkels@gmail.com>
2023-02-27 18:48:38 +0800

    added another question

--- chatBot/chatBot.java
+++ chatBot/chatBot.java
@@ -4,6 +4,8 @@ public class chatBot {
 	
 	public static void main (String[] args) {
 		System.out.println("Hi! I'm Emily, an interactive chatbot!");
+		String name = IBIO.input("What's your name? ");
+		System.out.println("Hi " + name + "! That's a really nice name!");
 		int bedtime = IBIO.inputInt("What time do you go to bed? (2400hrs format): ");
 		if(bedtime <= 2100){
 			System.out.println("That's a Healthy time to go to sleep! :D");
