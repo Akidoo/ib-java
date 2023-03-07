ee281a8480fa424f94b4529ebb847f3f8128026b
Shashwat <88643549+shash-b@users.noreply.github.com>
2023-03-07 23:09:44 +0800

    Update betterBinaryConverter.java
    Mult was always 0 and wasn't being updated.

--- betterBinaryConverter/betterBinaryConverter.java
+++ betterBinaryConverter/betterBinaryConverter.java
@@ -4,16 +4,15 @@ public class betterBinaryConverter {
 	
 	public static void main (String[] args) {
 		
-		int mult = 0;
+		int mult = 1;
 		int sum = 0;
 		int num = IBIO.inputInt("Input a binary number: ");
 		
-		for(int i = 0; i<=8; i++){
+		for(int i = 0; i<=8; i++, mult *= 2) {
 			sum += (num%2)*mult;
 			num = num/10;
-			
-			}
-			System.out.println(sum);
+		}
+		System.out.println(sum);
 		
 	}
 }
