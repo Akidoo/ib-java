7cb37c80d743b9531d678a04181bff831d284144
Akidoo <plunkels@gmail.com>
2023-03-28 23:48:06 +0800

    array clone

--- cloneArray/Clone.java
+++ cloneArray/Clone.java
@@ -1,7 +1,7 @@
+import java.util.Arrays;
 
 
-public class Replace {
-	
+public class Clone {
 	public static void printArray(String[] a)
 	{
 		System.out.println("Akaash Banerjee. " + "Array Length: " + a.length);
@@ -11,26 +11,22 @@ public class Replace {
 			
 			}
 	}
-	public static void replaceElement(String[] a)
+	
+	public static void cloneElement(String[] a)
 	{
-		String Replacement = "Annika";
-		for(int i = 0; i<a.length; i++){
-			if(a[i] == Replacement) {
-				
-				a[i] = "Anthony";
-				}
-			}
-			for(int f = 0; f<a.length; f++){
-			System.out.println(a[f]);
+    String[] newArray = Arrays.copyOf(a, a.length);
+    for (int i = 0; i < newArray.length; i++) {
+        System.out.println(newArray[i]);
+    }
+}
+
 		
-		}
-	}
 	
 	public static void main (String[] args) {
 		String array[] = {"Jaziel", "Annika", "Kimber", "Amelie", "Nikola", "Taliah", "Abdiel", "Fredy", "Zakai", "Karly"};
 		printArray(array);
-		replaceElement(array);
+		cloneElement(array);
+		
 	}
 }
-
 
