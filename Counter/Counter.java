aeb12d43c640b3067893a269bc31a0d734f351e7
Akidoo <plunkels@gmail.com>
2023-03-29 00:23:28 +0800

    counter update

---
+++ Counter/Counter.java
@@ -0,0 +1,11 @@
+import java.time.LocalTime;
+import java.time.Duration;
+
+
+public class Counter {
+	
+	public static void main (String[] args) {
+		 LocalDate date = LocalDate.now().minusDays(300);
+	}
+}
+
