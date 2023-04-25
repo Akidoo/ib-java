18cf8de72a7028a36005074fada6c97896da4bd1
Akidoo <plunkels@gmail.com>
2023-04-25 19:24:29 +0800

    selection sort

---
+++ selectionSort/selectionSort.java
@@ -0,0 +1,33 @@
+
+public class selectionSort {
+	
+		public static void main(String[] args) {
+		
+		int array[] = {8, 7, 9, 2, 3, 1, 5, 4, 6};
+		
+		selectionSort(array);
+		
+		for(int i : array) {
+			System.out.print(i);
+		}	
+	}
+
+	private static void selectionSort(int[] array) {
+		
+		for(int i = 0; i < array.length - 1; i++) {
+			int min = i;
+			for(int j = i + 1; j < array.length; j++) {
+				if(array[min] > array[j]) {
+					min = j;
+				}
+			}
+			
+			int temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+		}
+		
+	}
+}
+
+
