d9d1c0a336d37c214714ce7ee686f95a9d25abc6
Akidoo <plunkels@gmail.com>
2023-04-25 21:01:48 +0800

    Sorting Algos

---
+++ SortingAlgos/SortingAlgos.java
@@ -0,0 +1,86 @@
+
+public class SortingAlgos
+{
+	public static void printArray(int[] a)
+	{
+		for(int i = 0; i < a.length; i++)
+		{
+			System.out.printf( "[%d]%-2d  ",i , a[i]);
+		}
+		System.out.println();
+	}
+
+	public static int[] clone(int[] a)
+	{
+		int[] x = new int[a.length];
+		for(int i = 0; i < a.length; i++)
+		{	x[i] = a[i];
+		}
+		return x;
+	}
+
+	public static void selectionSort(int[] a)
+	{
+		int currentIndex = 0;
+		while( currentIndex < a.length - 1 )
+		{
+			int smallestIndex = currentIndex;
+			int smallestValue = a[smallestIndex];
+			int checkIndex	  = smallestIndex + 1;
+			while( checkIndex < a.length )
+			{
+				if(a[checkIndex] < smallestValue)
+				{
+					smallestIndex = checkIndex;
+					smallestValue = a[checkIndex];
+				}
+				checkIndex++;
+			}
+			a[smallestIndex] = a[currentIndex];
+			a[currentIndex] = smallestValue;
+			currentIndex++;
+			System.out.print("\t");
+			printArray(a);
+		}
+	}
+
+	public static void bubbleSort(int[] a)
+	{
+		for(int i = 0; i < a.length - 1; i++) {
+			for(int j = 0; j < a.length - i - 1; j++) {
+				if(a[j] > a[j+1]) {
+					int temp = a[j];
+					a[j] = a[j+1];
+					a[j+1] = temp;
+				}
+			}
+		}
+	}
+
+	public static void insertionSort(int[] a)
+	{
+		// To be done in class. Refer to sorting_algos_SL.pdf
+	}
+
+	public static void main (String[] args)
+	{
+		int[] original = { 12, 7, 14, 9, 5, 3 };
+		int[] selection = clone(original);
+		int[] bubble = clone(original);
+		int[] insert = clone(original);
+		System.out.println("\nOriginal array:");
+		printArray(original);
+		System.out.println("\nSelection sort:");
+		selectionSort(selection);
+		System.out.println("\nFinal result:");
+		printArray(selection);
+		System.out.println("\nBubble sort:");
+		bubbleSort(bubble);
+		System.out.println("\nFinal result:");
+		printArray(bubble);
+		System.out.println("\nInsertion sort:");
+		insertionSort(insert);
+		System.out.println("\nFinal result:");
+		printArray(insert);
+	}
+}
