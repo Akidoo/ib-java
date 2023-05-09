e1e1a2bd4b8b31e47648c0a2d0208b4c95f1bc50
Akaash Banerjee <plunkels@gmail.com>
2023-05-09 10:44:58 +0800

    Stacks

---
+++ Stacks/Stacks.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class Stacks {
+	
+	public static void main (String[] args) {
+		
+		
+		//FILO (First in last out) data structure
+		Stack<String> stack = new Stack<String>();
+		
+		stack.push("Minecraft");
+		stack.push("Pokemon");
+		stack.push("Undertale");
+		
+		System.out.println(stack.empty());
+		System.out.println(stack);
+		
+		//String myFavGame = stack.pop(); //Removes Undertale from the stack and stores it
+		System.out.println(stack.peek()); //Returns top most item within the stack
+		System.out.println(stack);
+		
+		System.out.println(stack.search("Pokemon")); //locates iten within stack
+		
+	}
+}
+
