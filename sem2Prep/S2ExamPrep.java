3651145a00076ac9f7d41f8ce8f148699a78be96
Akaash Banerjee <plunkels@gmail.com>
2023-05-11 12:00:11 +0800

    Finished assignment

--- sem2Prep/S2ExamPrep.java
+++ sem2Prep/S2ExamPrep.java
@@ -65,21 +65,26 @@ public class S2ExamPrep
 			}
 			return min;
 	}
-	/*
+	
 	public static double average(int[] array)
 	{
-		
+		double sum = 0;
+		double average;
+		for(int i = 0; i<array.length; i++){
+		sum = sum + array[i];
 	}
-*/
+	average = sum/array.length;
+	return average;
+	}
+
 	public static void main (String[] args)
 	{
 		int[] random = { 8, 6, 1, 9, 3, 6, 6, 8, 8, 8, 7, 9, 2, 5, 6, 7, 6, 6, 6, 7, 7, 8, 3, 3, 5, 5, 5, 8, 3, 5, 9, 4, 3, 7, 9, 9, 4, 7, 8, 3, 1, 9, 4, 6, 1, 1, 8, 2, 5, 3, 6, 9, 7, 1, 8, 3, 2, 8, 3, 8, 1, 9, 3, 3, 3, 8, 8, 4, 6, 4, 7, 5, 6, 6, 3, 2, 2, 2, 4, 7, 6, 2, 6, 3, 8, 5, 8, 3, 9, 9, 4, 8, 3, 6, 6, 4, 1, 3, 8, 6, 1, 1, 8, 3, 2, 9, 9, 2, 5, 5, 1, 3, 1, 9, 2, 4, 3, 3, 2, 2, 1, 3, 8, 4, 8, 2, 8, 1, 2, 8, 2, 5, 5, 4, 7, 6, 4, 1, 7, 4, 5, 1, 1, 9, 3, 8, 3, 9, 4, 2, 4, 7, 9, 9, 4, 2, 3, 4, 5, 8, 6, 9, 6, 6, 6, 5, 4, 7, 1, 4, 7, 2, 1, 6, 9, 1, 6, 7, 5, 4, 4, 1, 4, 2, 4, 2, 4, 5, 3, 9, 6, 6, 5, 5, 5, 1, 1, 7, 8, 5 };
 		
 		System.out.println( count(random) );
 		System.out.println( largest(random) );
-		
 		System.out.println( smallest(random) );
-		//System.out.println( average(random) );
+		System.out.println( average(random) );
 		
 	}
 }
