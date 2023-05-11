d33a2e78fc7b370410b3a79b7bb399d6c8f131c1
Akaash Banerjee <plunkels@gmail.com>
2023-05-11 10:59:26 +0800

    Update assignment

--- sem2Prep/S2ExamPrep.java
+++ sem2Prep/S2ExamPrep.java
@@ -14,7 +14,7 @@ public class S2ExamPrep
 	static int[] primes = { 2, 3, 5, 7 };
 	
 	// complete your methods here
-	public boolean isPrime(int n)
+	public static boolean isPrime(int n)
 	{
 		
 			for(int j = 0; j<primes.length; j++){
@@ -39,17 +39,33 @@ public class S2ExamPrep
 			}
 			return counter;
 	}
-	/*
+	
 	public static int largest(int[] array)
 	{
+		int max = primes[0];
 		
+		for(int i = 1; i<array.length; i++){
+			int temp = array[i];
+			if(temp>max && isPrime(temp)){
+				max = temp;
+				}
+			}
+			return max;
 	}
 	
 	public static int smallest(int[] array)
 	{
+		int min = primes[0];
 		
+		for(int i = 1; i<array.length; i++){
+			int temp = array[i];
+			if(temp<min && isPrime(temp)){
+				min = temp;
+				}
+			}
+			return min;
 	}
-	
+	/*
 	public static double average(int[] array)
 	{
 		
@@ -60,11 +76,11 @@ public class S2ExamPrep
 		int[] random = { 8, 6, 1, 9, 3, 6, 6, 8, 8, 8, 7, 9, 2, 5, 6, 7, 6, 6, 6, 7, 7, 8, 3, 3, 5, 5, 5, 8, 3, 5, 9, 4, 3, 7, 9, 9, 4, 7, 8, 3, 1, 9, 4, 6, 1, 1, 8, 2, 5, 3, 6, 9, 7, 1, 8, 3, 2, 8, 3, 8, 1, 9, 3, 3, 3, 8, 8, 4, 6, 4, 7, 5, 6, 6, 3, 2, 2, 2, 4, 7, 6, 2, 6, 3, 8, 5, 8, 3, 9, 9, 4, 8, 3, 6, 6, 4, 1, 3, 8, 6, 1, 1, 8, 3, 2, 9, 9, 2, 5, 5, 1, 3, 1, 9, 2, 4, 3, 3, 2, 2, 1, 3, 8, 4, 8, 2, 8, 1, 2, 8, 2, 5, 5, 4, 7, 6, 4, 1, 7, 4, 5, 1, 1, 9, 3, 8, 3, 9, 4, 2, 4, 7, 9, 9, 4, 2, 3, 4, 5, 8, 6, 9, 6, 6, 6, 5, 4, 7, 1, 4, 7, 2, 1, 6, 9, 1, 6, 7, 5, 4, 4, 1, 4, 2, 4, 2, 4, 5, 3, 9, 6, 6, 5, 5, 5, 1, 1, 7, 8, 5 };
 		
 		System.out.println( count(random) );
-		/*
 		System.out.println( largest(random) );
+		
 		System.out.println( smallest(random) );
-		System.out.println( average(random) );
-		*/
+		//System.out.println( average(random) );
+		
 	}
 }
 
