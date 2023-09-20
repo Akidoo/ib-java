1a4b3164aedc8793246de01abde153292ceed888
Akaash Banerjee <plunkels@gmail.com>
2023-09-20 15:10:33 +0800

    OOP Practice

---
+++ OOPPractice/BST/BST.java
@@ -0,0 +1,9 @@
+package OOPPractice.BST;
+
+public class BST {
+    int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    Node Parent = new Node();
+
+   
+}
---
+++ OOPPractice/BST/Node.java
@@ -0,0 +1,8 @@
+package OOPPractice.BST;
+
+public class Node {
+    Node Node;
+    Node leftNode;
+    Node rightNode;
+    
+}
